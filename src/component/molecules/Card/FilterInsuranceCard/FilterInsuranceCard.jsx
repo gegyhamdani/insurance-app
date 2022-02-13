ddfb8dd6cd44ff99ed0873232550e6ca4a1b8812
Gegy Faskal Nur Hamdani <gegyhamdani@gmail.com>
2022-02-13 19:35:21 +0700

    feat(product-list): add search input component

--- src/component/molecules/Card/FilterInsuranceCard/FilterInsuranceCard.jsx
+++ src/component/molecules/Card/FilterInsuranceCard/FilterInsuranceCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Select, Divider } from 'antd';
+import { Select, Divider, Input } from 'antd';
 import BaseCard from '../BaseCard';
 import {
   companyName,
@@ -13,43 +13,54 @@ const { Option } = Select;
 const FilterInsuranceCard = () => {
   return (
     <BaseCard className={styles['filter-insurance-card__wrapper']}>
-      <Select
-        defaultValue={companyName[0]}
-        style={{ width: 180 }}
-        bordered={false}
-      >
-        {companyName.map((val, i) => {
-          return (
-            <Option key={i.toString()} value={val.name} disabled={i === 0}>
-              {val.label}
-            </Option>
-          );
-        })}
-      </Select>
-      <Divider type="vertical" style={{ borderLeft: '2px solid #ddd' }} />
-      <Select
-        defaultValue={prizeRange[0]}
-        style={{ width: 200 }}
-        bordered={false}
-      >
-        {prizeRange.map((val, i) => {
-          return (
-            <Option key={i.toString()} value={val.name} disabled={i === 0}>
-              {val.label}
-            </Option>
-          );
-        })}
-      </Select>
-      <Divider type="vertical" style={{ borderLeft: '2px solid #ddd' }} />
-      <Select defaultValue={type[0]} style={{ width: 200 }} bordered={false}>
-        {type.map((val, i) => {
-          return (
-            <Option key={i.toString()} value={val.name} disabled={i === 0}>
-              {val.label}
-            </Option>
-          );
-        })}
-      </Select>
+      <div className={styles['filter-insurance-card__container']}>
+        <div className={styles['filter-insurance-card__content--select']}>
+          <Select
+            defaultValue={companyName[0]}
+            style={{ width: 180 }}
+            bordered={false}
+          >
+            {companyName.map((val, i) => {
+              return (
+                <Option key={i.toString()} value={val.name} disabled={i === 0}>
+                  {val.label}
+                </Option>
+              );
+            })}
+          </Select>
+          <Divider type="vertical" style={{ borderLeft: '2px solid #ddd' }} />
+          <Select
+            defaultValue={prizeRange[0]}
+            style={{ width: 200 }}
+            bordered={false}
+          >
+            {prizeRange.map((val, i) => {
+              return (
+                <Option key={i.toString()} value={val.name} disabled={i === 0}>
+                  {val.label}
+                </Option>
+              );
+            })}
+          </Select>
+          <Divider type="vertical" style={{ borderLeft: '2px solid #ddd' }} />
+          <Select
+            defaultValue={type[0]}
+            style={{ width: 200 }}
+            bordered={false}
+          >
+            {type.map((val, i) => {
+              return (
+                <Option key={i.toString()} value={val.name} disabled={i === 0}>
+                  {val.label}
+                </Option>
+              );
+            })}
+          </Select>
+        </div>
+        <div className={styles['filter-insurance-card__content--search']}>
+          <Input placeholder="Search by product name" />
+        </div>
+      </div>
     </BaseCard>
   );
 };
